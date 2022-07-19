45de373f38ffb9d3929d983cad73aa98057cf611
Amit Patel <redblobgames@gmail.com>
2022-07-18 17:20:40 -0700

    Fix: make randInt match python's closed interval; add randRange too
    My randInt function returned an integer in the half-open interval [a, b). But
    Python's randint function returns in the closed interval [a, b].
    Python's randrange is what returns the half-open [a, b), so I renamed
    my function to randRange.
    
    But I wanted to use randInt, so I added randInt to return the closed
    interval [a, b].

--- util.ts
+++ util.ts
@@ -8,8 +8,14 @@ export function clamp(x: number, lo: number, hi: number): number {
     return x < lo ? lo : x > hi ? hi : x;
 }
 
-export function randInt(lo: number, hi: number): number {
+/** Exclusive randInt, like python's */
+export function randRange(lo: number, hi: number): number {
     return Math.floor(Math.random() * (hi-lo)) + lo;
+}
+
+/** Inclusive randInt, like python's */
+export function randInt(lo: number, hi: number): number {
+    return randRange(lo, hi+1);
 }
 
 export function sign(v: number): -1 | 0 | 1 {
